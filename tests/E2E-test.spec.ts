055fde04959e22d25a7fbff06d7ae7794113615e
Utkarsh630 <shivamsr1998@gmail.com>
2026-03-27 01:56:07 +0530

    "update E2E test"

--- tests/E2E-test.spec.ts
+++ tests/E2E-test.spec.ts
@@ -1,6 +1,6 @@
 import {test, expect} from "@playwright/test";
 
-test("User should be able to login with valid credentials", async ({page})=>{
+test("User should be able to login with valid credentials and add product to cart", async ({page})=>{
     await page.goto("https://rahulshettyacademy.com/client");
     await page.locator("#userEmail").fill("ush@gmail.com");
     await page.locator("#userPassword").fill("Usha@1234");
@@ -10,4 +10,21 @@ test("User should be able to login with valid credentials", async ({page})=>{
 
     expect(msg).toContain("Login Successfully");
 
+    await page.waitForLoadState("networkidle");
+
+    const productCards = await page.locator('.card-body');
+
+    for(let i=0;i < await productCards.count();i++){
+        const productTitles = await productCards.locator("h5 b").allTextContents();
+        if(productTitles[i].trim() === "ZARA COAT 3"){
+            await productCards.nth(i).locator("text =  Add To Cart").click();
+            break;
+        }
+    }
+
+    await page.locator("[routerLink*='cart']").click();
+    await page.locator("div li").first().waitFor();
+
+    const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+    expect(bool).toBeTruthy();
 })
