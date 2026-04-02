3435d0e0889ae96fc0b4821b0e273335b409881f
Utkarsh630 <shivamsr1998@gmail.com>
2026-04-03 00:31:40 +0530

    added validations for calendar

--- tests/calendar.spec.ts
+++ tests/calendar.spec.ts
@@ -5,17 +5,24 @@ test("Calendar Validation", async({page})=>{
     await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
     
     const date = "15";
-    const currentMonth = "6";
-    const currentYear = "2027";
+    const month = "6";
+    const year = "2027";
+
+        const expectedList = [month,date,year];
+
    
 
     await page.locator(".react-date-picker__inputGroup").click();
     await page.locator(".react-calendar__navigation__label").click();
     await page.locator(".react-calendar__navigation__label").click();
 
-    await page.getByText(currentYear).click();
-    await page.locator(".react-calendar__year-view__months__month").nth(Number(currentMonth)-1).click();
+    await page.getByText(year).click();
+    await page.locator(".react-calendar__year-view__months__month").nth(Number(month)-1).click();
     await page.locator(`//abbr[text()='${date}']`).click();
+
+      await expect(page.locator('input[name="month"]')).toHaveValue(month);
+    await expect(page.locator('input[name="day"]')).toHaveValue(date);
+    await expect(page.locator('input[name="year"]')).toHaveValue(year);
 
 
 });
