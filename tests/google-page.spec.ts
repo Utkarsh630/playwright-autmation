2b0b35a38c10c3f767e95320969c62434d9dfa7f
Utkarsh630 <shivamsr1998@gmail.com>
2026-04-04 02:26:09 +0530

    google page verification

---
+++ tests/google-page.spec.ts
@@ -0,0 +1,12 @@
+import {test, expect} from '@playwright/test';
+
+test("Google App launcher", async({page})=>{
+
+    await page.goto("https://www.google.com");
+
+    await page.getByRole('button', {name: 'Google apps'}).click();
+
+    const drive = page.frameLocator("iframe[src*='ogs']").getByRole('link', {name: 'Drive'});
+    await expect(drive).toBeVisible();
+
+})
