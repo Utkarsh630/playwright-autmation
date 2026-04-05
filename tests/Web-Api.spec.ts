3e6a58342a06f6737b8255be7b2839920dd3fe8f
Utkarsh630 <shivamsr1998@gmail.com>
2026-04-06 00:51:49 +0530

    refactor code

--- tests/Web-Api.spec.ts
+++ tests/Web-Api.spec.ts
@@ -1,52 +1,51 @@
 import { test, expect, request } from "@playwright/test";
 import APIUtils from "./utils/apiUtils";
 
-const loginPayLoad = {userEmail:"ush@gmail.com",userPassword:"Usha@1234"};
-const orderPayLoad = {orders:[{country:"Cuba",productOrderedId:"6960eae1c941646b7a8b3ed3"}]};
-let response: {token: string, orderId: string};
-test.beforeAll( async ()=>{
-
-    const apiContext = await request.newContext()
-    const apiUtils = new APIUtils(apiContext, loginPayLoad);
-    response = await apiUtils.createOrder(orderPayLoad);
+const loginPayLoad = { userEmail: "ush@gmail.com", userPassword: "Usha@1234" };
+const orderPayLoad = {
+  orders: [{ country: "Cuba", productOrderedId: "6960eae1c941646b7a8b3ed3" }],
+};
+let response: { token: string; orderId: string };
+test.beforeAll(async () => {
+  const apiContext = await request.newContext();
+  const apiUtils = new APIUtils(apiContext, loginPayLoad);
+  response = await apiUtils.createOrder(orderPayLoad);
 });
-
 
 test("User should be able to login with valid credentials and add product to cart", async ({
   page,
 }) => {
-   await page.addInitScript(value =>{
-    window.localStorage.setItem('token', value);
-   }, response.token);   
-     await page.goto("https://rahulshettyacademy.com/client", {
+  await page.addInitScript((value) => {
+    window.localStorage.setItem("token", value);
+  }, response.token);
+  await page.goto("https://rahulshettyacademy.com/client", {
     waitUntil: "networkidle",
-  });   
+  });
 
   // to verify order in order history
 
-    await page.locator("button[routerLink*='myorders']").click();
+  await page.locator("button[routerLink*='myorders']").click();
 
-    await page.locator("tbody").waitFor();
+  await page.locator("tbody").waitFor();
 
-    const rows = page.locator(".table.table-hover tbody tr");
+  const rows = page.locator(".table.table-hover tbody tr");
 
-    const rowCount = await rows.count();
+  const rowCount = await rows.count();
 
-    console.log("Total rows in order history: " + rowCount);
-let found = false;
-    for (let i = 0; i < rowCount; i++) {
-        const cellText = await rows.nth(i).locator("th").textContent();
-        console.log(cellText);
-        if (response.orderId && cellText && response.orderId.includes(cellText)) {
-        console.log("Order found in order history");
-        found=true;
-        await rows.nth(i).locator("button").first().click();
-        break;
-        }
+  console.log("Total rows in order history: " + rowCount);
+  let found = false;
+  for (let i = 0; i < rowCount; i++) {
+    const cellText = await rows.nth(i).locator("th").textContent();
+    console.log(cellText);
+    if (response.orderId && cellText && response.orderId.includes(cellText)) {
+      console.log("Order found in order history");
+      found = true;
+      await rows.nth(i).locator("button").first().click();
+      break;
     }
-    expect(found).toBeTruthy();
+  }
+  expect(found).toBeTruthy();
 
-    const orderIdDetails = await page.locator(".col-text").textContent();    
-    expect(orderIdDetails?.trim()).toBe(response.orderId);
-
+  const orderIdDetails = await page.locator(".col-text").textContent();
+  expect(orderIdDetails?.trim()).toBe(response.orderId);
 });
